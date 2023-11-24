a68a0606852b7e9714548136613005eab850f9e8
lmaobeans <145195524+lmaobeans@users.noreply.github.com>
2023-11-24 16:31:29 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById('').innerHTML = Date()
+ var x = document.getElementById('lmao').innerHTML
+function myfunction(){
+    document.getElementById('lmao2').innerHTML = lmao
+}
+let hello = "hello world "
